91a39ccff327087fc5cbe2cb18e97ddc116ea0aa
Charly Laurent <charly.laurent@mergify.com>
2025-03-27 11:15:51 +0000

    leap year

--- LeapYear/leapYear.js
+++ LeapYear/leapYear.js
@@ -1,4 +1,13 @@
-function leapYear() {
-  return "";
+function isLeapYear(year) {
+  if (year % 400 === 0){
+    return true;
+  }
+  if (year % 100 === 0){
+    return false;
+  }
+  if (year % 4 === 0){
+    return true;
+  }
+  return false;
 }
-module.exports = leapYear;
+module.exports = isLeapYear;
--- LeapYear/leapYear.test.js
+++ LeapYear/leapYear.test.js
@@ -1,5 +1,17 @@
-const leapYear = require('./leapYear');
+const isLeapYear = require('./leapYear');
 
-test("leapYear returns empty string", () => {
-  expect(leapYear()).toBe("");
+test("2025 is not a Leap Year ", () => {
+  expect(isLeapYear(2025)).toBe(false);
+});
+
+test("Year divisible by 4 is a Leap Year ", () => {
+  expect(isLeapYear(2024)).toBe(true);
+});
+
+test("Year divisible by 100 is not a Leap Year ", () => {
+  expect(isLeapYear(1900)).toBe(false);
+});
+
+test("Year divisible by 400 is a Leap Year ", () => {
+  expect(isLeapYear(2000)).toBe(true);
 });
