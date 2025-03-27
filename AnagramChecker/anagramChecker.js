b34c2ac0a51dcd12520f981d6bc02a6d32edc964
Charly Laurent <charly.laurent@mergify.com>
2025-03-27 15:42:16 +0000

    anagram checker

--- AnagramChecker/anagramChecker.js
+++ AnagramChecker/anagramChecker.js
@@ -1,4 +1,16 @@
-function anagramChecker() {
-  return "";
+function anagramChecker(words1, words2) {
+  let sort_word1 = sort(clean(words1));
+  let sort_word2 = sort(clean(words2));
+
+  return sort_word1 === sort_word2;
 }
+
+function clean(words){
+  return words.toLowerCase().replace(/[^a-z]/g, '');
+}
+
+function sort(words) {
+  return words.split("").sort().join("");
+}
+
 module.exports = anagramChecker;
--- AnagramChecker/anagramChecker.test.js
+++ AnagramChecker/anagramChecker.test.js
@@ -1,5 +1,49 @@
 const anagramChecker = require('./anagramChecker');
 
-test("anagramChecker returns empty string", () => {
-  expect(anagramChecker()).toBe("");
+test("Check anagram for `toto` and `tata` returns false", () => {
+  // Arrange
+  let words1 = "toto";
+  let words2 = "tata";
+
+  // Act
+  let actual = anagramChecker(words1, words2);
+
+  // Assert
+  expect(actual).toBe(false);
+});
+
+test("Check anagram for `silent` and `listen` returns true", () => {
+  // Arrange
+  let words1 = "silent";
+  let words2 = "listen";
+
+  // Act
+  let actual = anagramChecker(words1, words2);
+
+  // Assert
+  expect(actual).toBe(true);
+});
+
+test("Check anagram for `anagram` and `nagaram` returns true", () => {
+  // Arrange
+  let words1 = "anagram";
+  let words2 = "nagaram";
+
+  // Act
+  let actual = anagramChecker(words1, words2);
+
+  // Assert
+  expect(actual).toBe(true);
+});
+
+test("Check anagram for `A            gentleman.!:` and `Elegant man` returns true", () => {
+  // Arrange
+  let words1 = "A            gentleman.!:";
+  let words2 = "Elegant man";
+
+  // Act
+  let actual = anagramChecker(words1, words2);
+
+  // Assert
+  expect(actual).toBe(true);
 });
