8fd241f77eb62398df67c20b4eda1dc9910343ee
Charly Laurent <charly.laurent@mergify.com>
2025-03-27 10:53:51 +0000

    fizzbuzz

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -1,4 +1,23 @@
-function fizzbuzz() {
-  return "";
+function fizzbuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  }
+  if (number % 3 === 0){
+    return "Fizz";
+  }
+  if (number % 5 == 0){
+    return "Buzz";
+  }
+  return number.toString();
 }
-module.exports = fizzbuzz;
+
+function main(n) {
+  let result = [];
+  for (let index = 1; index <= n; index++) {
+    const element = fizzbuzz(index);
+    result.push(element)
+  }
+  return result;
+}
+
+module.exports = {main, fizzbuzz};
--- FizzBuzz/fizzbuzz.test.js
+++ FizzBuzz/fizzbuzz.test.js
@@ -1,5 +1,45 @@
-const fizzbuzz = require('./fizzbuzz');
+const {main, fizzbuzz} = require('./fizzbuzz');
 
-test("fizzbuzz returns empty string", () => {
-  expect(fizzbuzz()).toBe("");
+test("fizzbuzz 1 returns 1", () => {
+  expect(fizzbuzz(1)).toBe("1");
+});
+
+test("fizzbuzz 3 returns Fizz", () => {
+  expect(fizzbuzz(3)).toBe("Fizz");
+});
+
+test("fizzbuzz 5 returns Buzz", () => {
+  expect(fizzbuzz(5)).toBe("Buzz");
+});
+
+test("fizzbuzz 6 returns Fizz", () => {
+  expect(fizzbuzz(6)).toBe("Fizz");
+});
+
+test("fizzbuzz 10 returns Buzz", () => {
+  expect(fizzbuzz(10)).toBe("Buzz");
+});
+
+test("fizzbuzz 15 returns FizzBuzz", () => {
+  expect(fizzbuzz(15)).toBe("FizzBuzz");
+});
+
+test("main(n=15) returns 15 FizzBuzz elements", () => {
+  expect(main(15)).toStrictEqual([
+       "1",
+       "2",
+       "Fizz",
+       "4",
+       "Buzz",
+       "Fizz",
+       "7",
+       "8",
+       "Fizz",
+       "Buzz",
+       "11",
+       "Fizz",
+       "13",
+       "14",
+       "FizzBuzz",
+     ]);
 });
