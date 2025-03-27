0bc1165374ef8986e869a2573530d073ceba0709
Charly Laurent <charly.laurent@mergify.com>
2025-03-27 14:23:33 +0000

    string calculator

--- StringCalculator/stringCalculator.js
+++ StringCalculator/stringCalculator.js
@@ -1,4 +1,21 @@
-function stringCalculator() {
-  return "";
+function stringCalculator(numbers) {
+  if (numbers) {
+    if (numbers.startsWith("//")) {
+      return add(numbers.slice(4), numbers[2]);
+    }
+    return add(numbers, /,|\n/);
+  }
+  return 0;
 }
+
+function add(numbers, delimiter) {
+
+  let arrayNumbers = numbers.split(delimiter).map(Number)
+
+  if(arrayNumbers.some(v => v < 0)){
+    throw new Error;  
+  }
+  return arrayNumbers.reduce((a, b) => a + b, 0);
+}
+
 module.exports = stringCalculator;
--- StringCalculator/stringCalculator.test.js
+++ StringCalculator/stringCalculator.test.js
@@ -1,5 +1,25 @@
 const stringCalculator = require('./stringCalculator');
 
-test("stringCalculator returns empty string", () => {
-  expect(stringCalculator()).toBe("");
+test("stringCalculator empty string returns 0 ", () => {
+  expect(stringCalculator("")).toBe(0);
+});
+
+test("stringCalculator 2 returns 2 ", () => {
+  expect(stringCalculator("2")).toBe(2);
+});
+
+test("stringCalculator 1,1 returns 2 ", () => {
+  expect(stringCalculator("1,1")).toBe(2);
+});
+
+test("stringCalculator 1\n2,3 returns 6", () => {
+  expect(stringCalculator("1\n2,3")).toBe(6);
+});
+
+test("stringCalculator //:\n1:2:3 returns 6", () => {
+  expect(stringCalculator("//:\n1:2:3")).toBe(6);
+});
+
+test("stringCalculator -1,2,3 returns Exception", () => {
+  expect(() => stringCalculator("-1,2,3")).toThrow(Error);
 });
